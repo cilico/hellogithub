45257ef63c8faf0871f51769a1fa9f04b2b965e6
cilico <you@example.com>
2016-12-07 14:52:37 +0800

    提交测试

---
+++ TestPrint/obj/local/armeabi/objs/hardware-print/sam.o.d
@@ -0,0 +1,196 @@
+obj/local/armeabi/objs/hardware-print/sam.o: jni/sam.c \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/fcntl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/fcntl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/fcntl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/fcntl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/unistd.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/select.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysconf.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/capability.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pathconf.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/thread_db.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h \
+ jni/debug.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/termios.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/ioctl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/ioctl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/ioctl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/ioctl.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/ioctls.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/termbits.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/ioctl_compat.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/termios.h \
+ I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/termios.h \
+ jni/sam.h
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/fcntl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/fcntl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/fcntl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/fcntl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/unistd.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/select.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysconf.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/capability.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pathconf.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/thread_db.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h:
+
+jni/debug.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/termios.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/ioctl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/ioctl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/ioctl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/ioctl.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/ioctls.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/termbits.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/ioctl_compat.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/termios.h:
+
+I:/Software/Eclipse_Windows/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/termios.h:
+
+jni/sam.h:
